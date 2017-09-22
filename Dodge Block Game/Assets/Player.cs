7fa28424da806d7dfec7ceace7024fd4c08b8e27
Ze120 <32185205+Ze120@users.noreply.github.com>
2017-09-23 04:21:02 +0700

    Fix map width

--- Dodge Block Game/Assets/Player.cs
+++ Dodge Block Game/Assets/Player.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float speed = 15f;
+    public float mapWidth = 5f;
 
     private Rigidbody2D rb;
 
     void Start () {
 
-        rb = GetComponent<Rigidbody2D>();
+        // Call this rigidbody sprite infomation 
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
     void FixedUpdate () {
 
-        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+        // Get input type horizontal and defind movement sprite
+        float horizontalMove = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
 
-        rb.MovePosition(rb.position + (Vector2.right * x));
+        // Store value next position sprite after get input 
+        Vector2 newPosition = rb.position + (Vector2.right * horizontalMove);
+
+        // Defind range sprite position in X-axis (Map Width) 
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+        
+        rb.MovePosition(newPosition);
     
     }
 }
