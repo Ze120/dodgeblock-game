022cf99c9c38e0ea38ed5509b7321229006961d2
Ze120 <32185205+Ze120@users.noreply.github.com>
2017-09-23 05:00:30 +0700

    Add spawn point

---
+++ Dodge Block Game/Assets/BlockSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+
+    public Transform[] spawnPoints;
+
+    public GameObject blockPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++) {
+
+            if (randomIndex != i) {
+
+                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+            
+            }
+        
+        }
+        
+	}
+	
+	
+}
