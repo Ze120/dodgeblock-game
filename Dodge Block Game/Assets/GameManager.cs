247fc4e185816ea8f3fe1b6cfc9439f344734812
Ze120 <32185205+Ze120@users.noreply.github.com>
2017-09-23 07:11:36 +0700

    Add slow down effect
    - using Coroutine
    - add slow down effect when we hit object and load new scene

--- Dodge Block Game/Assets/GameManager.cs
+++ Dodge Block Game/Assets/GameManager.cs
@@ -1,11 +1,31 @@
 ﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+    public float slowDownFactor = 10f;
+
     public void EndGame () {
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        StartCoroutine(RestartLevel());
 
+    }
+
+    IEnumerator RestartLevel () {
+
+        // Slow down the time with variable "slowDownFactor" time
+        Time.timeScale = 1f / slowDownFactor;
+        //Fix Time.fixedDeltaTime get the same scale time what we modify
+        Time.fixedDeltaTime = Time.fixedDeltaTime / slowDownFactor;
+
+        yield return new WaitForSeconds(1f / slowDownFactor);
+
+        // Set the time back to regular time
+        Time.timeScale = 1f;
+        // Set Time.fixedDeltaTime back to regular time
+        Time.fixedDeltaTime = Time.fixedDeltaTime * slowDownFactor;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
