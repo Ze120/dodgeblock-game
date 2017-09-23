af49f89e1de3c113ac3d6812250cba319c7a6709
Ze120 <32185205+Ze120@users.noreply.github.com>
2017-09-23 07:32:11 +0700

    Add block speed up

--- Dodge Block Game/Assets/Block.cs
+++ Dodge Block Game/Assets/Block.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
+    public float gravityScale = 25f;
+
+    void Start () {
+
+        GetComponent<Rigidbody2D>().gravityScale += Time.timeSinceLevelLoad / gravityScale;
+
+    }
+
 	// Update is called once per frame
 	void Update () {
 
